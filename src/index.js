fd655b952fde82652000055ea941efdf352715bb
dyyyl <hey@dyyyl.rocks>
2020-01-07 18:26:15 +0100

    remove unnecessary compiled file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var fp_1 = require("lodash/fp");
-var ramda_1 = require("ramda");
-var addOneLodash = fp_1.add(1);
-var addedArrayLodash = [1, 2, 3].map(addOneLodash);
-console.log({ lodash: addedArrayLodash });
-var addedArrayRamda = ramda_1.map(ramda_1.add(1), [1, 2, 3]);
-console.log({ ramda: addedArrayRamda });
